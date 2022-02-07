8746cf298f99ae6b8268fbbd02c66c3ff4fed352
Shineka <99110851+TheShineka@users.noreply.github.com>
2022-02-07 10:10:37 +0530

    Update manage.py

--- SpamprotectShineka/modules/manage.py
+++ SpamprotectShineka/modules/manage.py
@@ -111,7 +111,7 @@ async def nsfw_scan_command(_, message: Message):
     file_id = get_file_id(reply)
     if not file_id:
         return await m.edit("Something went wrong.")
-    file = await spr.download_media(file_id)
+    file = await SpamprotectShineka.download_media(file_id)
     try:
         results = await arq.nsfw_scan(file=file)
     except Exception as e:
